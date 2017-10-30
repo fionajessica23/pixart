a829ea47299d995808a9fc5f435c5cf0979cebca
Fiona Jessica <fionajessica23@gmail.com>
2017-10-31 10:06:35 +1100

    sorry, forgotten to pull request

--- pixart.js
+++ pixart.js
@@ -1,26 +1,81 @@
-var $setColorBtn = $('#set-color');
-var $colorField = $('#color-field');
+var $setTemplateBtn = $('#set-template');
 var $brushId = $('#brush-id');
 
-$setColorBtn.on('click', function() {
-  event.preventDefault();
+var checkColorOrMovie = function () {
+    event.preventDefault();
+    var colorOrMovieRadio = $('input[name=selection]:checked').val();
+    var optionValue = $('input[name=selection-value]').val();
 
-  var newColorValue = $colorField.val();
-  console.log(newColorValue);
-  $brushId.css("background-color", newColorValue)
-});
+    if (colorOrMovieRadio === 'color') {
+      $brushId.css("background-color", optionValue);
+    } else {
+      ajaxCall(optionValue);
+    }
+};
 
-$(document).ready(function () {
-  var $newdiv;
+var ajaxCall = function (movieName) {
+  var options = {
+    url: "http://omdbapi.com/",
+    method: 'get',
+    data: { apikey: '2f6435d9', t: movieName }
+  }
+  var appendMovie = function(movie){
+    $brushId.css("background-image", 'url('+movie.Poster+')');
+  }
+  $.ajax( options ).done( appendMovie );
+}
+
+var clickedSquare = function() {
+
+  $('.square').on('mouseover', function(event) {
+
+    var $clickedSquare = $(event.target);
+    var colorOrMovieRadio = $('input[name=selection]:checked').val();
+
+    if (colorOrMovieRadio === 'color') {
+      var $brushColor = $brushId.css("background-color");
+      $clickedSquare.css("background-color", $brushColor);
+
+    } else {
+      var $brushColor = $brushId.css("background-image");
+      $clickedSquare.css({"background-image": $brushColor, "background-position": "center center", "background-repeat": "no-repeat", "background-size": "cover"});
+
+    }
+    $clickedSquare.css({"height": "10px", "width": "10px", "margin": "0"});
+  });
+};
+
+var initSquare = function() {
+  var $newdiv = '';
   for (var i = 0; i < 1000; i++) {
     $newdiv = $('<div class="square">');
     $('body').append($newdiv);
-  }
-  $('.square').on('mouseover', function(event) {
-    var $clickedSquare = $(event.target);
-    // $clickedSquare.css("background-color", "green")
-    var $brushColor = $('.brush').css("background-color");
-    $clickedSquare.css("background-color", $brushColor);
-    $clickedSquare.css({"height": "10px", "width": "10px", "margin": "0"});
+  };
+};
+
+var enteredSelectionValue = function () {
+
+  $('input[name=selection-value]').keypress(function (e) {
+
+    if(e.which == 13) {
+      checkColorOrMovie();
+      //return false;
+    }
   });
+};
+
+var clickTemplateBtn = function () {
+
+  $setTemplateBtn.on('click', function() {
+    checkColorOrMovie();
+    // return false;
+  });
+};
+
+$(document).ready(function () {
+  initSquare();
+  clickedSquare();
+  checkColorOrMovie();
+  enteredSelectionValue();
+  clickTemplateBtn();
 });
